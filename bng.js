6833df97f1f4d604ea195ec8590e377fafb61a4f
AaronGavendo <agavendo@hotmail.com>
2015-02-23 19:00:18 -0500

    #2 Have a test button display text from js

---
+++ bng.js
@@ -0,0 +1,15 @@
+$(document).ready(function () //Function to run when website is loaded
+{
+    
+    
+    
+    
+    
+    $("#testButt").click(function () //For testing
+    {
+        console.log(99);
+        document.getElementById("outputText").innerHTML = "Test Text!";
+    });
+
+
+});
